c6901f163025128ee863ed8600e4ff6172cb4bab
Robert Ryan <ryanre@gmail.com>
2022-11-17 13:22:26 -0800

    release 2

--- palindrome/src/App.jsx
+++ palindrome/src/App.jsx
@@ -1,18 +1,38 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
 print = console.log;
+
+
 function App() {
+
+  const [isPalindrome, setIsPalindrome] = useState('');
 
   const handleChange = () => {
     const inputElement = document.getElementById('input');
     const outputElement = document.getElementById('output');
     outputElement.innerHTML = inputElement.value;
+
+    if (inputElement.value == inputElement.value.split('').reverse().join('')) {
+      setIsPalindrome(true)
+    } else {
+      setIsPalindrome(false)
+    }
+
   }
 
   return (
     <div className="App">
+      <Header text={'Palindrome'} />
       <input id='input' onChange={handleChange}></input>
-      <h3 id='output'></h3>
+      <p><span id='output'></span> 
+      {
+        isPalindrome === ''
+        ? ''
+        : isPalindrome
+        ? ' is a palindrome' 
+        : ' is not a palindrome'
+      }</p>
     </div>
   )
 }
---
+++ palindrome/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.text}</h1>
+      <hr />
+    </>
+  )
+}
+
+export default Header;
